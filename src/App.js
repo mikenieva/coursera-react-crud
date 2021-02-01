2b508d4fd36b5c9ef4e10a37564a157eb3662227
Mike Nieva <m@mikenieva.com>
2021-01-31 20:54:03 -0600

    Task final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
 import Main from './components/Main'
+import Header from './components/Header'
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Main/>
     </div>
   );
